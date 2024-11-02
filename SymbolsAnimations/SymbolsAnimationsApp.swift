903111a88cf6e23876652d7baccc846fd977b408
edy alves <30.edyalves@gmail.com>
2024-11-02 11:55:45 -0300

    Initial Commit

---
+++ SymbolsAnimations/SymbolsAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolsAnimationsApp.swift
+//  SymbolsAnimations
+//
+//  Created by ednardo alves on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolsAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
